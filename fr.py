5d253ece4f113c2d4d014d665b0f6238b05865a7
MahmoudAH <mah199645@gmail.com>
2017-03-06 15:10:04 +0200

    first commit

---
+++ fr.py
@@ -0,0 +1,89 @@
+#def main():
+ #print("welcome from python")
+#if __name__ == '__main__':main()
+#n="mahmoud"
+#print (n,type(n))
+#def main():
+ #num=int(30/6)
+ #print(num,type(num))
+
+#if __name__ == '__main__':main()
+
+  #amount=88
+  #data="{} is hight or {} is low or {} f".format(amount,88,1)
+  #data="my name is khan"
+  #print(data[11])
+
+  #age=(20,55,4,1,4)
+  #print(age[0:65])
+#def main():
+ # age=[20,45,85,45,4,4
+#age.append(20)
+ # age.insert(2,35)
+  #print(age[1:5])
+#if __name__ == '__main__':main((
+#def main():
+  #student={'name':"mahmoud ali hussein",'age':21,'salary':0}
+  #print(student,type(student))
+  #print(student['name'],student['age'])
+
+    #    print(student)
+#if __name__ == '__main__':main()
+#def main():
+ #s=['add','fdff','ff']
+ #s+='jgfd'
+ #s.insert(1,'ali')
+ #s.remove('ff')
+ #del s[1]
+ #s+=[',nxj','','']
+ #s.extend(['',''])#to append the 2 list
+ #s.reverse()
+ #s.sort()
+ #s+=['ff','ff']
+ #s.index("ff")
+
+ #print(s)
+
+  #if __name__ == '__main__':main()
+
+#bitwise{binary/hexa/decimal}
+#boolean{true/false}
+#priority
+#slic[lixt---l[:]=range(100)]
+#debug
+
+
+#to count your age
+#import datetime
+#def main():
+ #old=input("enter your age:")
+ #naw=datetime.datetime.now().year
+ #myage=naw-int(old)                     #to convert string to integer use int(old)
+ #print("my age is={}".format(myage))
+#if __name__ == '__main__':main()
+#
+# help()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
